4c55d372b95a49d99730ba9a98349de28585f056
Leonardo Perez <contact@leonardo-perez.me>
2023-10-31 23:59:03 -0500

    feat: create extension func for app viewmodels

---
+++ app/src/main/java/com/github/ephelsa/brightwheelexercise/utils/ActivityExt.kt
@@ -0,0 +1,11 @@
+package com.github.ephelsa.brightwheelexercise.utils
+
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import com.github.ephelsa.brightwheelexercise.di.DIModule
+
+internal inline fun <reified T : ViewModel> ComponentActivity.appViewModels(): Lazy<T> =
+    viewModels {
+        DIModule.viewModelFactory()
+    }
